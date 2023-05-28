e434db0f0f5e665b23fbe5d6f6223a81c681439b
Елизавета Носкова <wliza68@gmail.com>
2023-05-28 12:32:46 +0300

    дз выполнено

---
+++ src/main/java/AppConfiguration.java
@@ -0,0 +1,31 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    Car car(){
+        return new Car("Porsche","S",3.3);
+    }
+    @Bean
+    Bus bus(){
+        return new Bus("Mercedes","A", 4);
+    }
+    @Bean
+    Pickup pickup(){
+        return new Pickup("UAZ","X",6);
+    }
+
+    @Bean
+    Driver driverCar(){
+        return new Driver("Никита",car());
+    }
+    @Bean
+    Driver driverBus(){
+        return new Driver("Стас",bus());
+    }
+    @Bean
+    Driver driverPickup(){
+        return new Driver("Максим",pickup());
+    }
+}
---
+++ src/main/java/Car.java
@@ -0,0 +1,10 @@
+public class Car extends Transport{
+    public Car(String brand, String model, double engineCapacity) {
+        super(brand, model, engineCapacity);
+    }
+
+    public void start() {
+        System.out.println("Автомобиль готов к поездке");
+    }
+
+}
